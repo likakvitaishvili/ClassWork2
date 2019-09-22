98807d669ebeaf70e3ab8e30594a6259d38c595c
likakvitaishvili <51286363+likakvitaishvili@users.noreply.github.com>
2019-09-22 16:19:00 +0400

    Update main.java

--- main.java
+++ main.java
@@ -34,3 +34,32 @@ System.out.println("Jami = " + (a+b+c));
 System.out.println("Namravli = "+(a*b*c));
   }
 }
+
+//4
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Sheikvane samnishna ricxvi: ");
+Scanner input=new Scanner(System.in);
+int number=input.nextInt();
+String str = Integer.toString(number);
+for(int i=0;i<str.length();i++){
+System.out.println(str.charAt(i));
+}}}
+
+//5
+
+
+//6
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+  Scanner input=new Scanner(System.in);
+ int num=input.nextInt();
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        System.out.println(sum); 
+}}
