92fd9e1d98bd9a0fde4d16d5f5906e859036b945
likakvitaishvili <51286363+likakvitaishvili@users.noreply.github.com>
2019-09-22 15:30:05 +0400

    main.java

---
+++ main.java
@@ -0,0 +1,36 @@
+//1
+public class Main {
+  public static void main(String[] args) {
+System.out.println("C++");
+System.out.println("Java");
+System.out.println("C#");
+System.out.println("pascal");
+System.out.println("PHP");
+System.out.println("JavaScript");
+System.out.println("ActionScript");
+} }
+
+//2
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+Scanner input=new Scanner(System.in);
+int a=input.nextInt();
+int b=input.nextInt();
+System.out.println("Ganayofi = "+(a/b));
+System.out.println("Nashti = "+(b%a));
+  }
+}
+
+//3
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+Scanner input=new Scanner(System.in);
+int a=input.nextInt();
+int b=input.nextInt();
+int c=input.nextInt();
+System.out.println("Jami = " + (a+b+c));
+System.out.println("Namravli = "+(a*b*c));
+  }
+}
